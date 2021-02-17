bfd43beca1e7180cc4ae495225a8a5c88fa3ae9a
max <scraptrashscrap@gmail.com>
2021-02-17 19:29:53 +0100

    Initial commit

---
+++ src/com/practice/Main.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+/*Implement the list of cities to visit using
+1. Linkedlists
+2. Iterative concept instead for loops or enhaced for loops
+* it should have the following functionalities
+* 0 quit
+* 1 go to next city
+* 2 go to prev city
+* 4 print menu options
+* */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
