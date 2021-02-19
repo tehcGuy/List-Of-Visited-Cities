1371ee1c82b1adab39e1d09d018148389d88b4fe
max <scraptrashscrap@gmail.com>
2021-02-19 18:38:10 +0100

    implementing alphibaticallyAdd()

--- src/com/practice/Main.java
+++ src/com/practice/Main.java
@@ -14,56 +14,30 @@ import java.util.*;
 
 public class Main {
     public Scanner userChoise = new Scanner(System.in);
-    public ArrayList<String> linkedList = new ArrayList<>();
+    public LinkedList<String> linkedList = new LinkedList<>();
 
 
     public static void main(String[] args) {
         Main traveler1 = new Main();
         traveler1.loadToList(traveler1.linkedList);
+        alphibaticallyAdd(traveler1.linkedList, "b");
+        alphibaticallyAdd(traveler1.linkedList, "a");
         traveler1.printCities(traveler1.linkedList);
 
-        boolean exit = false;
-        while(!exit){
-            System.out.println("Please enter a digit to choose an option: ");
-            int choice = traveler1.userChoise.nextInt();
-
-            switch(choice){
-                case 0:
-                    exit = true;
-                    break;
-                case 1:
-                    traveler1.nextCity(traveler1.linkedList);
-                case 2:
-                    traveler1.prevCity(traveler1.linkedList);
-                case 3:
-
-                case 4:
-                    break;
-            }
-        }
     }
 
-    private static void menu() {
-        System.out.println("Please enter a digit to select one of the following options:");
-        System.out.println("0 -> quit");
-        System.out.println("1 -> visit next city");
-        System.out.println("2 -> visit prev city");
-        System.out.println("3 -> print all cities from your list");
-        System.out.println("4 -> print this menu");
-        System.out.println("9 -> load the default cities to visit to the list");
+
+    private static void loadToList(LinkedList<String> linkedList) {
+        alphibaticallyAdd(linkedList, "Poznan");
+        alphibaticallyAdd(linkedList, "Gdansk");
+        alphibaticallyAdd(linkedList, "Szczeczin");
+        alphibaticallyAdd(linkedList, "Warszawa");
+        alphibaticallyAdd(linkedList, "Lodz");
+        alphibaticallyAdd(linkedList, "Wroclaw");
     }
 
-    private static void loadToList(ArrayList<String> city) {
-        city.add("Poznan");
-        city.add("Gdansk");
-        city.add("Szczeczin");
-        city.add("Warszawa");
-        city.add("Lodz");
-        city.add("Wroclaw");
-    }
-
-    private void printCities(ArrayList<String> city) {
-        ListIterator<String> iterator = city.listIterator();
+    private static void printCities(LinkedList<String> city) {
+        Iterator<String> iterator = city.iterator(); //to print cities we only need an iterator instead listiterator!
         int i = 1;
         while(iterator.hasNext()) {
             System.out.println(i + ". " + iterator.next());
@@ -71,33 +45,40 @@ public class Main {
         }
     }
 
-//    here should be the function that shows that alphabetically adds Srting city name to the list
+    private static boolean alphibaticallyAdd(LinkedList<String> listOfCities, String cityName) {
+        ListIterator<String> listIterator = listOfCities.listIterator(); //just a setup; doesn't point to the 1st element
 
-    //    here should be the function that goes next to alphibetical ordered city
-    private void nextCity(ArrayList<String> city) {
-        ListIterator<String> iterator = city.listIterator();
-        System.out.println("You are in " + iterator.next());
+        while((listIterator.hasNext())) {
+            //if value is greater, the word that is in the list is alphabetically bigger, thus, put it before the list element
+            //if equal, it is duplicate! return false
+            // else it is less, thus, we have to move further in the list
+            int comparison = listIterator.next().compareTo(cityName); //retrieves the 1st value and goes to the next
+            if(comparison == 0) {
+                System.out.println(cityName + " has been already added to the list");
+                return false;
+            } else if(comparison > 0) {
+                listIterator.previous(); //because we've used .next() in the int comparison initialization
+                listIterator.add(cityName); //dont use linkedList.add because it doesnt know the int comparison, so cannot properly add!!!
+                return true;
+            }
+
+        }
+        listIterator.add(cityName); //adding at the end of the list
+        return true;
     }
 
-    private void prevCity(ArrayList<String> city) {
-        ListIterator<String> iterator = city.listIterator();
-        System.out.println("You are in " + iterator.previous());
+    private static void menu() {
+        System.out.println("Please enter a digit to select one of the following options:");
+        System.out.println("0 -> stop program");
+        System.out.println("1 -> visit next city");
+        System.out.println("2 -> visit prev city");
+        System.out.println("3 -> print all cities from your list");
+        System.out.println("4 -> print this menu");
+        System.out.println("5 -> add a city");
+        System.out.println("6 -> removing a city");
+
+        System.out.println("9 -> load the default cities to visit to the list");
     }
-
-
-//    here should be the function that goes back to alphibetical ordered city
-
 }
 
-/* WORKS
-        ListIterator<String> iterator = linkedList.listIterator();
-        int i = 1;
-        while(iterator.hasNext()){
-            System.out.println(i +". "+iterator.next());
-            i++;
-        }
-it is equivalent to
-System.out.println(linkedList);
 
-
-*/
